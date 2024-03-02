99a8657e70edabf1d29979a0355e4fac6b4d3fb2
tonyking <tony@king.com>
2024-03-02 11:03:37 +1100

    Runnable stage 2 - init data

--- src/entity/Product.ts
+++ src/entity/Product.ts
@@ -1,4 +1,7 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm'
+import { Category } from './Category'
+import { Subcategory } from './Subcategory'
+import { Uom } from './Uom'
 
 @Entity()
 export class Product {
@@ -11,4 +14,15 @@ export class Product {
     @Column()
     description: string
 
+    // Product has one Category
+    @ManyToOne(() => Category, category => category.products, { nullable: false })
+    category: Category
+
+    // Product can have one Subcategory
+    @ManyToOne(() => Subcategory, subcategory => subcategory.products, { nullable: true })
+    subcategory: Subcategory
+
+    // Product has one UOM
+    @ManyToOne(() => Uom, uom => uom.products, { nullable: false })
+    uom: Uom
 }
